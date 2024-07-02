4dc76e2b643be701b2c2bbc42471bf1c95971054
milad babanajmi <40021606+miladbn@users.noreply.github.com>
2024-07-02 10:40:57 +0330

    longest-palindromic-substring.ts added

---
+++ 5.longest-palindromic-substring/longest-palindromic-substring.ts
@@ -0,0 +1,45 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const longestPalindrome = (s: string): string => {
+  if (s.length === 0) return "";
+
+  const t = "$" + s.split("").join("$") + "$";
+  const n = t.length;
+  const p: number[] = Array(n).fill(0);
+  let center = 0;
+  let right = 0;
+
+  let maxLength = 0;
+  let maxCenter = 0;
+
+  for (let i = 0; i < n; i++) {
+    const mirror = 2 * center - i;
+
+    if (i < right) {
+      p[i] = Math.min(right - i, p[mirror]);
+    }
+
+    while (
+      i + p[i] + 1 < n &&
+      i - p[i] - 1 >= 0 &&
+      t[i + p[i] + 1] === t[i - p[i] - 1]
+    ) {
+      p[i]++;
+    }
+
+    if (i + p[i] > right) {
+      center = i;
+      right = i + p[i];
+    }
+
+    if (p[i] > maxLength) {
+      maxLength = p[i];
+      maxCenter = i;
+    }
+  }
+
+  const start = Math.floor((maxCenter - maxLength) / 2);
+  return s.substring(start, start + maxLength);
+};
