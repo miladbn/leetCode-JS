ceb55b4ffc70ec30cce849dec8290b1a2e01d302
milad babanajmi <40021606+miladbn@users.noreply.github.com>
2024-07-03 17:35:26 +0330

    two sum best memory

---
+++ 1.two-sum/best-memory/two-sum.ts
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums: number[], target: number): number[] => {
+  let op: number[] = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target && i !== j && !op.includes(i)) {
+        op.push(i);
+        op.push(j);
+      }
+    }
+  }
+  return op;
+};
